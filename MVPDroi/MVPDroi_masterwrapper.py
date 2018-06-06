ff43474799f87e5f6129853d8672a95b57f21145
YichenLi666 <yl3506@nyu.edu>
2018-06-06 12:20:05 -0400

    first commit

---
+++ MVPDroi/MVPDroi_masterwrapper.py
@@ -0,0 +1,30 @@
+### main execution function
+
+## environment and input setup
+# import necessary libraries and variables
+# specify the input, and parameters
+
+
+## preprocessing and region models
+# processing model, reduce noise
+# save the result to new input folders
+
+
+## seperate the dataset by 3 parts
+## set ouput folders
+
+## regularization
+# fix lambda
+# determine which pair to train
+# train with part A to find the prediction function
+
+
+## validation and performance
+# test the prediction function with part B to find error performnace
+# save the performance into a list
+
+
+## compare the performance of each prediction function
+## test the selected parameters with part C
+
+
---
+++ MVPDroi/generate_script.py
@@ -0,0 +1,8 @@
+## generate parallel scripts and submit to queue
+
+# import necessary data
+import MVPDroi_masterwrapper
+
+# specify parameters
+subject_num = [1,2,3,4,5,6,9,10,14,15,16,17,18,19,20]
+n_subject = len(subject_num)

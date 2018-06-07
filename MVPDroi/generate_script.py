d8c998d535f6884a3dd6ce9fc5bde9eaf079c75e
YichenLi666 <yl3506@nyu.edu>
2018-06-07 11:24:35 -0400

    sbatch script parameters

--- MVPDroi/generate_script.py
+++ MVPDroi/generate_script.py
@@ -4,5 +4,16 @@
 import MVPDroi_masterwrapper
 
 # specify parameters
-subject_num = [1,2,3,4,5,6,9,10,14,15,16,17,18,19,20]
-n_subject = len(subject_num)
+all_subjects = [1,2,3,4,5,6,9,10,14,15,16,17,18,19,20]
+n_subject = len(all_subjects)
+n_analysis = 
+mvpd_dir = "/mindhive/saxelab3/anzellotti/software/" # TBD
+parameters = {}
+parameters['slurm'] = {}
+parameters['slurm']['name'] = 'MVPDroi_iModels_facesVoices'
+parameters['slurm']['time'] = 5 # in days
+parameters['slurm']['cores_per_node'] = 5
+parameters['slurm']['cpus_per_task'] = 1
+parameters['slurm']['mem_per_cpu'] = 10240
+parameters['slurm']['email'] = 'xxx@mit.edu'
+

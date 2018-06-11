65c6348626fcfc956883047fd132a65b53da321b
yl3506 <yl3506@nyu.edu>
2018-06-11 14:58:33 -0400

    load data initialize

--- MVPDroi/master.py
+++ MVPDroi/master.py
@@ -7,6 +7,7 @@ import os
 data_directory = '/Users/chloe/Documents/data_test/'
 num_subject = 2 # total number of subjects
 all_sub_list = [] # list of all subjects
+[0.01, 0.03, 0.1, 0.3, 1.0, 3.0]
 
 # iterate through subject combinations and load model
 os.chdir(data_directory)
@@ -15,7 +16,6 @@ for data_file in all_data_files:
 	# filter to select subject folders only
 	if 'sub-' in data_file:
 		all_sub_list.append(data_file)
-
 
 
 ## preprocessing and region models

fdc61be870b0c3eca28a4a5883f98646cc1fe811
yl3506 <yl3506@nyu.edu>
2018-06-15 11:53:39 -0400

    cross-subject linear regression testing

--- test.py
+++ test.py
@@ -1,24 +1,31 @@
 import os
 import nibabel as nib
 import numpy as np
-# load data
+import json
+from sklearn import linear_model
+
+# load data and mask
 os.chdir('/Users/chloe/Documents/data_test/')
 mask = nib.load('aal.nii.gz')
 mask_data = mask.get_data()
-img = nib.load('sub-rid000001_task-beh_run-1_bold_space-MNI152NLin2009cAsym_preproc.nii.gz')
-img_data = img.get_data()
+img_a = nib.load('sub-rid000001_task-beh_run-1_bold_space-MNI152NLin2009cAsym_preproc.nii.gz')
+img_b = nib.load('sub-rid000012_task-beh_run-1_bold_space-MNI152NLin2009cAsym_preproc.nii.gz')
+img_a_data = img_a.get_data()
+img_b_data = img_b.get_data()
 print("mask_data shape")
 print(mask_data.shape)
-print("img_data shape")
-print(img_data.shape)
+print("img_a_data shape")
+print(img_a_data.shape)
+print("img_b_data shape")
+print(img_b_data.shape) # a and b should be of the same shape
 
-# rearrange: select sub-array of mask to match shape of img
+# resize mask: select sub-matrix to match shape of img
 mask_data=mask_data[50:115,100:177,80:145]
-print("mask_data shape after rearrangement")
+print("mask_data shape after resize")
 print(mask_data.shape)
 # check number of voxels with certain label
-mask_a_label = 79
-print("number of label 79") # 316
+mask_a_label = 8
+print("number of label 8") # 1324
 num_label_a = np.count_nonzero(mask_data==mask_a_label)
 print(num_label_a)
 mask_b_label = 2
@@ -43,22 +50,22 @@ for t in range(0, 196):
 		for y in range(0, 77):
 			for z in range(0, 65):
 				if mask_a[x,y,z]: # if mask true
-					print("xyzt, a_index: ")
-					print(x)
-					print(y)
-					print(z)
-					print(t)
-					print(a_index)
-					a[t, a_index] = img_data[x,y,z,t]
+					print("xyzt, a_index: %d" % a_index)
+					print('x = %d' % x)
+					print('y = %d' % y)
+					print('z = %d' % z)
+					print('t = %d' % t)
+					print('a = %f' % img_a_data[x, y, z, t])
+					a[t, a_index] = img_a_data[x, y, z, t]
 					a_index += 1
-				elif mask_b[x,y,z]:
-					print("xyzt, b_index: ")
-					print(x)
-					print(y)
-					print(z)
-					print(t)
-					print(b_index)
-					b[t, b_index] = img_data[x,y,z,t]
+				if mask_b[x,y,z]:
+					print("xyzt, b_index: %d" % b_index)
+					print('x = %d' % x)
+					print('y = %d' % y)
+					print('z = %d' % z)
+					print('t = %d' % t)
+					print('b = %f' % img_b_data[x, y, z, t])
+					b[t, b_index] = img_b_data[x, y, z, t]
 					b_index += 1				
 	a_index = 0 # reset column index
 	b_index = 0
@@ -68,4 +75,25 @@ for t in range(0, 196):
 print(a)
 print(b)
 
+# linear regression step
+# split datasets into training and testing sets
+a_train = a[:150, :]
+a_test = a[150:, :] 
+b_train = b[:150, :]
+b_test = b[150:, :]
+# check if the split is in correct size
+print(a_train.shape)
+print(a_test.shape)
+# initialize linear regression model
+regr = linear_model.LinearRegression() # with default settings
+# fit in training sets
+regr.fit(a_train, b_train)
+# get coefficients of the training resutl
+print(regr.coef_)
+# testing
+predict = regr.predict(a_test)
+# check with the answer and calculate error (squared)
+err = predict - b_test
+print('squared error: %f' % np.sum(err * err))
+print('b_test * b_test: %f' % np.sum(b_test * b_test))
 

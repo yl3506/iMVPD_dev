4afd9a520b5255a5ee47618f397ead4c01ed0447
yl3506 <yl3506@nyu.edu>
2018-06-19 18:40:33 -0400

    test on forrest within-subject, different shape

--- test.py
+++ test.py
@@ -3,51 +3,56 @@ import nibabel as nib
 import numpy as np
 import json
 from sklearn import linear_model
+
 # load data and mask
-os.chdir('/Users/chloe/Documents/data_test/')
+os.chdir('/Users/chloe/Documents/')
 mask = nib.load('aal.nii.gz')
 mask_data = mask.get_data()
-img_a = nib.load('sub-rid000001_task-beh_run-1_bold_space-MNI152NLin2009cAsym_preproc.nii.gz')
-img_b = nib.load('sub-rid000012_task-beh_run-1_bold_space-MNI152NLin2009cAsym_preproc.nii.gz')
+img_a = nib.load('sub-18_ses-movie_task-movie_run-1_bold_space-T1w_preproc.nii.gz')
+img_b = nib.load('sub-17_ses-movie_task-movie_run-1_bold_space-T1w_preproc.nii.gz')
 img_a_data = img_a.get_data()
 img_b_data = img_b.get_data()
 print("mask_data shape")
 print(mask_data.shape)
+img_a_data_shape = img_a_data.shape
 print("img_a_data shape")
 print(img_a_data.shape)
+img_b_data_shape = img_b_data.shape
 print("img_b_data shape")
-print(img_b_data.shape) # a and b should be of the same shape
+print(img_b_data.shape) # a and b might be of different shape
 
 # resize mask: select sub-matrix to match shape of img
-mask_data=mask_data[50:115,100:177,80:145]
+mask_data_a=mask_data[80:129,100:161,80:128]
+mask_data_b=mask_data[80:128,98:155,81:126]
 print("mask_data shape after resize")
-print(mask_data.shape)
+print(mask_data_a.shape)
+print(mask_data_b.shape)
 # check number of voxels with certain label
 mask_a_label = 8
-print("number of label 8") # 1324
-num_label_a = np.count_nonzero(mask_data==mask_a_label)
+print("number of label 8") 
+num_label_a = np.count_nonzero(mask_data_a==mask_a_label)
 print(num_label_a)
 mask_b_label = 2
-print("number of label 2") # 1383
-num_label_b = np.count_nonzero(mask_data==mask_b_label)
+print("number of label 2") 
+num_label_b = np.count_nonzero(mask_data_b==mask_b_label)
 print(num_label_b)
 
 # generate mask
-mask_a = mask_data == mask_a_label
-mask_b = mask_data == mask_b_label
+mask_a = mask_data_a == mask_a_label
+mask_b = mask_data_b == mask_b_label
 
 # apply mask on img_data
-a = np.zeros((196,num_label_a+1)) # initialize region a
-b = np.zeros((196,num_label_b)) # initialize region b
-# iterate through img_data with mask
+a = np.zeros((img_a_data_shape[3],num_label_a+1)) # initialize region a
+b = np.zeros((img_b_data_shape[3],num_label_b)) # initialize region b
+
+# iterate through img_a_data with mask_a
 a_index = 0
-b_index = 0
-for t in range(0, 196):
+for t in range(0, img_a_data_shape[3]):
 	a[t, a_index] = 1 # bias term
 	a_index += 1
-	for x in range(0, 65):
-		for y in range(0, 77):
-			for z in range(0, 65):
+	for x in range(0, img_a_data_shape[0]):
+		for y in range(0, img_a_data_shape[1]):
+			for z in range(0, img_a_data_shape[2]):
 				if mask_a[x,y,z]: # if mask true
 					print("xyzt, a_index: %d" % a_index)
 					print('x = %d' % x)
@@ -56,7 +61,15 @@ for t in range(0, 196):
 					print('t = %d' % t)
 					print('a = %f' % img_a_data[x, y, z, t])
 					a[t, a_index] = img_a_data[x, y, z, t]
-					a_index += 1
+					a_index += 1				
+	a_index = 0 # reset column index
+
+# iterate through img_b_data with mask_b
+b_index = 0
+for t in range(0, img_b_data_shape[3]):
+	for x in range(0, img_b_data_shape[0]):
+		for y in range(0, img_b_data_shape[1]):
+			for z in range(0, img_b_data_shape[2]):
 				if mask_b[x,y,z]:
 					print("xyzt, b_index: %d" % b_index)
 					print('x = %d' % x)
@@ -66,9 +79,7 @@ for t in range(0, 196):
 					print('b = %f' % img_b_data[x, y, z, t])
 					b[t, b_index] = img_b_data[x, y, z, t]
 					b_index += 1				
-	a_index = 0 # reset column index
-	b_index = 0
-
+	b_index = 0 # reset column index
 
 # check if region data is correctly masked
 print(a)
@@ -76,10 +87,10 @@ print(b)
 
 # linear regression step
 # split datasets into training and testing sets
-a_train = a[:150, :]
-a_test = a[150:, :] 
-b_train = b[:150, :]
-b_test = b[150:, :]
+a_train = a[:img_a_data_shape[3] - 50, :]
+a_test = a[img_a_data_shape[3] - 50:, :] 
+b_train = b[:img_b_data_shape[3] - 50, :]
+b_test = b[img_b_data_shape[3] - 50:, :]
 # check if the split is in correct size
 print(a_train.shape)
 print(a_test.shape)
@@ -99,7 +110,7 @@ print('b_test * b_test: %f' % np.sum(b_test * b_test))
 
 # now try regularization model
 clf = linear_model.MultiTaskElasticNetCV()
-encv.fit(a_train, b_train)
+clf.fit(a_train, b_train)
 predict_clf = clf.predict(a_test)
 err_clf = b_test - predict_clf
 print("squared error: %f" % np.sum(err_clf * err_clf))

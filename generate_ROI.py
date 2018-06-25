8f0d92c300457404cfcc4b940eda83caab6d3faa
yl3506 <yl3506@nyu.edu>
2018-06-25 16:50:55 -0400

    fill in structure

--- generate_ROI.py
+++ generate_ROI.py
@@ -1,4 +1,4 @@
-import os 
+import os, math
 import nibabel as nib
 import numpy as np
 
@@ -15,6 +15,7 @@ mask_shape = mask.shape
 
 # initiate final mask
 final_mask = np.zeros(mask_shape)
+temp_mask = np.zeros(mask_shape)
 
 # iterate through data and find peak
 max_x, max_y, max_z = 0, 0, 0
@@ -27,6 +28,7 @@ for x in range(0, data_shape[0]):
 				max_z = z
 
 # mark peak as true
+temp_mask[max_x, max_y, max_z] = 1
 final_mask[max_x, max_y, max_z] = 1
 
 # make a sphere around the peak
@@ -34,7 +36,28 @@ r = 9 # radius of sphere in mm
 x_mm = 2.8 # mm in each voxel x dimension
 y_mm = 2.831168831168831 # mm in each voxel y dimension
 z_mm = 3.033333333333333 # mm in each voxel z dimension
-for 
+for x in range(max_x - 4, max_x + 4):
+	for y in range(max_y - 4, max_y + 4):
+		for z in range(max_z - 4, max_z + 4):
+			distance = math.sqrt(math.pow((max_x - x) * x_mm, 2) + math.pow((max_y - y) * y_mm, 2) + math.pow((max_z - z) * z_mm, 2))
+			if distance <= r:
+				temp_mask[x, y, z] = 1
 
 # select max 80 voxels to be the real mask
+count = 80
+max_list = []
+for x in range(max_x - 4, max_x + 4):
+	for y in range(max_y - 4, max_y + 4):
+		for z in range(max_z - 4, max_z + 4):
+			if temp_mask[x, y, z] == 1:
+				max_list.append(data[x, y, z])
+
+max_list.sort(reverse = True)
+max_list = max_list[:count]
+for x in range(max_x - 4, max_x + 4):
+	for y in range(max_y - 4, max_y + 4):
+		for z in range(max_z - 4, max_z + 4):
+			if temp_mask[x, y, z] == 1 and data[x, y, z] in max_list:
+				final_mask[x, y, z] = 1
+
 

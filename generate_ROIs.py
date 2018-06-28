1dc2cf7a5225dea972093ecb1b605344dc0ce4b7
yl3506 <yl3506@nyu.edu>
2018-06-28 16:01:26 -0400

    refine

--- generate_ROIs.py
+++ generate_ROIs.py
@@ -4,26 +4,28 @@ import numpy as np
 
 # initialize parameters
 work_directory = '/Users/chloe/Documents/'
-# work_directory = '/mindhive/saxelab3/anzellotti/forrest/'
-# all_subjects = ['sub-01', 'sub-02', 'sub-03', 'sub-04', 'sub-05', 'sub-09', 'sub-10', 'sub-14', 'sub-15', 'sub-16', 'sub-17', 'sub-18', 'sub-19', 'sub-20']
-all_subjects = ['sub-19']
+all_subjects = ['sub-19', 'sub-20']
+### work_directory = '/mindhive/saxelab3/anzellotti/forrest/'
+### all_subjects = ['sub-01', 'sub-02', 'sub-03', 'sub-04', 'sub-05', 'sub-09', 'sub-10', 'sub-14', 'sub-15', 'sub-16', 'sub-17', 'sub-18', 'sub-19', 'sub-20']
 all_masks = ['TPmask_R_funcSize_bin.nii.gz', 'ROI_rFFA_kanparcel_xyz_warped_funcSize_bin.nii.gz', 'ROI_rOFA_kanparcel_xyz_warped_funcSize_bin.nii.gz', 'wholeROI_rSTS_kanparcel_warped_funcSize_bin.nii.gz']
 mask_label = 0
-#all_masks_dir = ''
+### all_masks_dir = '/mindhive/saxelab3/anzellotti/forrest/mask_tests/kanparcel_nii/'
 all_masks_dir = '/Users/chloe/Documents/kanparcel_nii/'
 r = 9 # radius of sphere in mm
 x_mm = 2.8 # mm in each voxel x dimension
 y_mm = 2.831168831168831 # mm in each voxel y dimension
 z_mm = 3.033333333333333 # mm in each voxel z dimension
 final_voxel_count = 80 # total number of voxels needed for each ROI
+final_mask_name = ''
 
 os.chdir(work_directory)
 
 for subject in all_subjects: # iterate through all subjects
 	
 	# data initialization
-	sub_dir = work_directory + 'derivatives/fmriprep/' + subject + '_complete/'
-	out_dir = work_directory + 'derivatives/fmriprep/' + subject + '_complete/' + subject + '_ROIs/'
+	### sub_dir = work_directory + 'derivatives/fmriprep/' + subject + '_complete/'
+	sub_dir = work_directory + subject + '_complete/'
+	out_dir = sub_dir + subject + '_ROIs/'	
 	# create output directory if not exists
 	if not os.path.exists(out_dir):
 		os.makedirs(out_dir)
@@ -40,8 +42,8 @@ for subject in all_subjects: # iterate through all subjects
 		mask_label += 1 # increment mask label number
 		mask_dir = all_masks_dir + mask
 		mask_data = nib.load(mask_dir) # load mask data
-		mask_data = mask_data.get_data()
 		mask_data_affine = mask_data.affine
+		mask_data = mask_data.get_data()
 		mask_data_shape = mask_data.shape
 		
 		# determine final mask output file name
@@ -53,6 +55,10 @@ for subject in all_subjects: # iterate through all subjects
 			final_mask_name = 'rOFA_final_mask_' + subject + '_bin.nii.gz'	
 		elif mask_label == 4:
 			final_mask_name = 'rSTS_final_mask_' + subject + '_bin.nii.gz'
+		else:
+			continue
+
+		print(final_mask_name)
 
 		# initiate final mask
 		final_mask = np.zeros(mask_data_shape)
@@ -63,7 +69,7 @@ for subject in all_subjects: # iterate through all subjects
 		for x in range(0, zstat3_data_shape[0]):
 			for y in range(0, zstat3_data_shape[1]):
 				for z in range(0, zstat3_data_shape[2]):
-					if mask_data[x, y, z] == 1 and zstat3_data[x, y, z] > stat3_data[max_x, max_y, max_z]:
+					if mask_data[x, y, z] == 1 and zstat3_data[x, y, z] > zstat3_data[max_x, max_y, max_z]:
 						max_x = x
 						max_y = y
 						max_z = z

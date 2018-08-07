a56217d030c3b31550b9cf282ac5f04cf9933b23
yl3506 <yl3506@nyu.edu>
2018-08-06 23:45:37 -0400

    compcorr

--- visualize/visualize_vs_raw_overall.py
+++ visualize/visualize_vs_raw_overall.py
@@ -7,9 +7,9 @@ import itertools as it
 # initialize parameters
 ### work_dir = '/Users/chloe/Documents/output_denoise_pca_1_cross/'
 ### main_out_dir = '/Users/chloe/Documents/'
-work_dir = '/mindhive/saxelab3/anzellotti/forrest/output_cos_pc3_v3/'
+work_dir = '/mindhive/saxelab3/anzellotti/forrest/output_cos_global_compcorr_pc3_v3/'
 main_out_dir = '/mindhive/saxelab3/anzellotti/forrest/'
-out_dir = main_out_dir + 'vs_cos_pc3_noATL_v3.png'
+out_dir = main_out_dir + 'vs_cos_global_compcorr_pc3_noATL_v3.png'
 #all_subjects = ['sub-01', 'sub-02', 'sub-03', 'sub-04', 'sub-05', 'sub-09', 'sub-10', 'sub-14', 'sub-15', 'sub-16', 'sub-17', 'sub-18', 'sub-19', 'sub-20']
 all_subjects = ['sub-01', 'sub-02', 'sub-04', 'sub-05', 'sub-09', 'sub-15', 'sub-16', 'sub-17', 'sub-18', 'sub-19', 'sub-20']
 all_masks = ['rOFA', 'rFFA', 'rSTS', 'rTOS', 'rPPA', 'rPC']
@@ -58,6 +58,6 @@ plt.xticks(np.arange(len(all_masks)), all_masks) # set x axis tick
 plt.yticks(np.arange(len(all_masks)).T, all_masks) # set y axis tick
 plt.colorbar() # show color bar
 plt.ylabel('Predictor') # set y axis label
-plt.title('within-cross var explained raw, decos, pc=3', y=title_y) # set title
+plt.title('within-cross var expl raw, cos+glob+compcorr, pc=3', y=title_y) # set title
 plt.xlabel('Target', labelpad=labelpad_x) # set x axis label
 plt.savefig(out_dir) # save figure

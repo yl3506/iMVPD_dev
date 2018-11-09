082c41afef93f07a7c67ccffcc96a5962ec1631f
yl3506 <yl3506@nyu.edu>
2018-11-09 16:29:49 -0500

    .

--- accuracy/accuracy_corr_sub_pca.py
+++ accuracy/accuracy_corr_sub_pca.py
@@ -56,13 +56,13 @@ X2 = pca2.transform(X) # output X2:[11,11] = [n_samples, n_components]
 # XX2 is the projection of the approximated data XX onto the same PCA space
 XX2 = pca2.transform(XX) # output XX2: [11,11] = [n_samples, n_components]
 # now we calculate the varexp for each column (feature/region-pair)
-var = np.zeros((1,len(all_subjects))) # [1,11] varexp for each column
-for i in range(len(all_subjects)):
+var = np.zeros((1,X2.shape(1))) # [1,11] varexp for each column
+for i in range(X2.shape(1)):
 	# varexp for each column/dimension/region-pair
 	var[0,i] = 1 - np.var(X2[:,i] - XX2[:,i]) / np.var(X2[:,i])
 # now we calculate the total varexp of the original data explained by the 2 components together
 total_var = 0
-for i in range(len(all_subjects)): # iterate through each column
+for i in range(X2.shape(1)): # iterate through each column
 	# collect the varexp for each dimension/feature/region-pair
 	total_var += pca.explained_variance_ratio_[i] * var[0,i]
 # print total varexp of the original data explained by the 2 components together
